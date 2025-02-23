282678c8f2f77d02bf9be12400f998333affb80b
Shaista <shiastasiddique@gmail.com>
2025-02-23 08:09:03 +0500

    sss

---
+++ script.js
@@ -0,0 +1,61 @@
+// var array = [1, 2, 3, 4, 5]
+// array.forEach(function(value)
+// {
+//     console.log(value + " hello")
+// } )
+
+// var array = [2, 4, 6, 8]
+// var newarray = array.map(function(value){
+//     return value + 2
+// }
+// )
+// console.log(newarray)
+
+// var array = [1, 3, 5, 7, 9]
+// var newarray = array.filter(function(value){
+//     if(value > 3){
+//         return true
+//     }else{
+//         return false
+//     }
+// })
+// console.log(newarray)
+
+// var obj = {
+//     name: "shaista",
+//     age: 22
+// }
+// object.freeze(obj)
+
+// obj.name = "aliya"
+
+// function abcd(){
+//     return "hello world"
+// }
+
+// var newarray = abcd();
+
+
+// async function fetchData() {
+//     try {
+//         // Fetch the data from the API
+//         const api = await fetch("https://my.demio.com/api/v1/ping");
+
+//         // Check if the request was successful (status code 200)
+//         if (!api.ok) {
+//             throw new Error(`HTTP error! Status: ${api.status}`);
+//         }
+
+//         // Parse the JSON response
+//         const api2 = await api.json();
+
+//         // Log the response
+//         console.log(api2);
+//     } catch (error) {
+//         // Catch and log any errors that occur
+//         console.error('Error fetching data:', error);
+//     }
+// }
+
+// // Call the async function
+// fetchData();
